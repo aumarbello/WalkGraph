4ca8fe45af171b7ed2cbff5d4d2e55e5e04277b4
Ahmed Umar <khattabu@gmail.com>
2017-11-09 12:15:03 +0100

    Refactored SplashFragment and SplashFragmentImpl

--- app/src/main/java/com/example/ahmed/walkgraph/presentation/splash/SplashFragment.java
+++ app/src/main/java/com/example/ahmed/walkgraph/presentation/splash/SplashFragment.java
@@ -2,8 +2,13 @@ package com.example.ahmed.walkgraph.presentation.splash;
 
 /**
  * Created by ahmed on 8/9/17.
+ *
+ * MVP Contract for SplashFragment.
  */
 
 public interface SplashFragment {
+    /**
+     * Switches to MapFragment
+     */
     void openMapFragment();
 }
--- app/src/main/java/com/example/ahmed/walkgraph/presentation/splash/SplashFragmentImpl.java
+++ app/src/main/java/com/example/ahmed/walkgraph/presentation/splash/SplashFragmentImpl.java
@@ -15,22 +15,45 @@ import javax.inject.Inject;
 
 /**
  * Created by ahmed on 8/9/17.
+ *
+ * Fragment to represent splashScreen.
  */
 
 public class SplashFragmentImpl extends Fragment implements SplashFragment{
+    /**
+     * CallBack interFace to be implemented by containing activity.
+     */
     public interface SplashCallback {
+        /**
+         * Switch to map.
+         */
         void changeToMap();
     }
 
+    /**
+     * Fields
+     */
     @Inject
     SplashPresenterImpl splashPresenter;
     private SplashCallback callback;
+
+    /**
+     * Initializes injected fields, Prevents NPE.
+     * @param savedInstance of the fragment.
+     */
     @Override
     public void onCreate(Bundle savedInstance){
         super.onCreate(savedInstance);
         ((App)getActivity().getApplication()).getComponent().inject(this);
     }
 
+    /**
+     * Inflate the resource this fragment.
+     * @param inflater to use in inflating layout resource.
+     * @param container to pass to inflate method.
+     * @param savedInstanceState of the fragment.
+     * @return the view for this fragment.
+     */
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -38,6 +61,11 @@ public class SplashFragmentImpl extends Fragment implements SplashFragment{
         return inflater.inflate(R.layout.fragment_splash, container, false);
     }
 
+    /**
+     * Instantiates the Callback field from the containing activity.
+     * @param context passed from the containing activity.
+     * @throws RuntimeException when the activity does not implement the callback interface.
+     */
     @Override
     public void onAttach(Context context){
         super.onAttach(context);
@@ -48,6 +76,9 @@ public class SplashFragmentImpl extends Fragment implements SplashFragment{
             throw new RuntimeException(context.toString() + "Must implement SplashCallback");
     }
 
+    /**
+     * Switches to MapFragment
+     */
     @Override
     public void openMapFragment() {
         callback.changeToMap();
